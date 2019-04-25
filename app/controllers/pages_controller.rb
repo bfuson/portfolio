a6b0656a129d97c23bacda1956c22def073d24f0
William Fuson <williamfuson@Bills-iMac.local>
2019-04-24 21:37:43 -0400

    Integrated skills via model generator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     @posts = Blog.all  # store all blog posts in the instance variable @posts
+    @skills = Skill.all   #  look at the skills data
   end
 
   def about
