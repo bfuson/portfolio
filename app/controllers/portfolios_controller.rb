851235d97de4d2fe3a44a5fbf4735b21c2ea1b6e
William Fuson <williamfuson@Bills-iMac.local>
2019-04-30 17:14:30 -0400

    more code cosmetic changes while debugging missing portfolio_item edit methods. Still not working

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,23 +8,23 @@ class PortfoliosController < ApplicationController
       @portfolio_item = Portfolio.new      #  get the Portfolio information for display
     end
     
-      def create
+    def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))   
                                   # this passes parameters, and specifies the permitted parms that can be passed to the app
-
-  # POST /portfolio_item
-  # POST /portfolio_item.json
-    respond_to do |format|
-      if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your Portfolio item is now live.' }
-        format.json { render :show, status: :created, location: @portfolio_item }
-      else
-        format.html { render :new }
-        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-end
+        # POST /portfolio_item
+        # POST /portfolio_item.json
+       respond_to do |format|
+         if @portfolio_item.save
+           format.html { redirect_to portfolios_path, notice: 'Your Portfolio item is now live.' }  #  adds to the full list of portfolio_items, not the show page
+           format.json { render :show, status: :created, location: @portfolio_item }   # displays on the show page?
+         else
+           format.html { render :new }
+           format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+         end    #  end if...else...block
+      end   #  end respond_to do......   block
+   end    #  end Portfolios create method
+   
+end   # end class PortfoliosController definition
 
 def edit 
   @portfolio_item = Portfolio.find(params[:id])   # :id is the record number in the database.
