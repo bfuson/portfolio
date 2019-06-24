6abc2818fd3cbd560e75a74f02e4eae476f4e858
William Fuson <williamfuson@Bills-iMac.local>
2019-06-24 15:06:26 -0400

    added new parameters for user registration and editing account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+   protect_from_forgery with:  :exception   
+                                      # not in the generated Controller, so added as in the class work
+   before_action :configure_permitted_parameters, if: :devise_controller?
+              # :configure_permitted_parameters   -   created just for the devcamp class, only available if defined in the application
+              #  if:  :devise_controller?   -  means that this will be used only if active comm's with the devise controller
+   def configure_permitted_parameters
+     # these are specific to Rails 5
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+   end
 end
