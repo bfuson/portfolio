2b827e85a2b2717a512c01e88022e42757c59834
William Fuson <williamfuson@Bills-iMac.local>
2019-06-24 22:43:45 -0400

    Completed authentication with virtural attributes for user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
    protect_from_forgery with:  :exception   
                                       # not in the generated Controller, so added as in the class work
    before_action :configure_permitted_parameters, if: :devise_controller?
+              #   before_action was before_filter ,  newer version of RAILS has apparently deprecated before_filter.
               # :configure_permitted_parameters   -   created just for the devcamp class, only available if defined in the application
               #  if:  :devise_controller?   -  means that this will be used only if active comm's with the devise controller
    def configure_permitted_parameters
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,24 @@ class User < ApplicationRecord
   # created by command:     Bills-iMac:DevcampPortfolio williamfuson$ rails g devise:views
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+     
+     
+   validates_presence_of :name
+      # security protection to ensure a name field / value is present
+          
+   def first_name
+     # extract first name from the name field
+     self.name.split.first    
+        # split the name field into an array of values separated by spaces or specified argument such as a comma
+        # returns the first element of the array
+        #  if name = "John Smith"
+        #  self.split.name  ==>  ["John","Smith"]
+        #  self.split.name.first  ==>   "John"
+     
+   end
+   
+   def last_name
+     self.name.split.last 
+   end
+   
 end
