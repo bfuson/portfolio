8e05ce3eb2599b16f8e3f34ca80eba5bf7c045be
William Fuson <williamfuson@Bills-iMac.local>
2019-08-07 22:56:17 -0400

    Implemented view helper for auth links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def sample_helper
+    "<p>My helper</p>".html_safe
+          # html_safe asserts that the html code entered here is safe and can be processed
+          # leaving the "html_safe" assertion off will prevent the html from being processed, 
+          # instead it will be displayed exactly as typed.  In this case,
+          #  <p>My helper</p>
+  end
+  
+  def login_helper
+    if current_user.is_a?(User)
+       link_to "Logout", destroy_user_session_path, method: :delete
+    else
+       (link_to "Register", new_user_registration_path) +
+       "<br>".html_safe + 
+       (link_to "Login", new_user_session_path)
+    end
+  end
+  
 end
